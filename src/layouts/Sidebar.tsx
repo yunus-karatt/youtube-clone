c1abeb2aa7a7bb5c7fbf8268d88f6d4338186479
yunus <yyas404@gmail.com>
2024-01-06 17:36:01 +0530

    landing page

---
+++ src/layouts/Sidebar.tsx
@@ -0,0 +1,74 @@
+import { Clapperboard, Home, Library, Repeat } from "lucide-react";
+import { ElementType } from "react";
+import { buttonStyles } from "../components/Button";
+import { twMerge } from "tailwind-merge";
+
+export function Sidebar() {
+  return (
+    <>
+      <aside className="sticky top-0 overflow-y-auto scrollbar-hidden pb-4 flex flex-col ml-1">
+        <SmallSidebarItem Icon={Home} title="Home" url="/" />
+        <SmallSidebarItem Icon={Repeat} title="Shorts" url="/shorts" />
+        <SmallSidebarItem
+          Icon={Clapperboard}
+          title="Subscriptions"
+          url="/subscriptions"
+        />
+        <SmallSidebarItem Icon={Library} title="Library" url="/library" />
+      </aside>
+      <aside className="w-56 lg:sticky absolute top-0 overflow-y-auto scrollbar-hidden pb-4 flex-col gap-2 px-2 flex">
+        <LargeSidebarSection>
+          <LargeSidebarItem isActive icon={Home} title="Home" url="/" />
+        </LargeSidebarSection>
+      </aside>
+    </>
+  );
+}
+
+type SmallSidebarItemProps = {
+  Icon: ElementType;
+  title: string;
+  url: string;
+};
+
+function SmallSidebarItem({ Icon, title, url }: SmallSidebarItemProps) {
+  return (
+    <a
+      href={url}
+      className={twMerge(
+        buttonStyles({ variant: "ghost" }),
+        "py-4 px-1 flex flex-col items-center rounded-lg gap-1"
+      )}
+    >
+      <Icon className="w-6 h-6" />
+      <div className="text-sm">{title}</div>
+    </a>
+  );
+}
+
+
+function LargeSidebarSection(){
+
+}
+
+type LargeSidebarItemProps={
+  Icon: ElementType;
+  title: string;
+  url: string;
+  isActive?: boolean
+}
+
+function LargeSidebarItem({Icon, title, url}:LargeSidebarItemProps){
+  return (
+    <a
+      href={url}
+      className={twMerge(
+        buttonStyles({ variant: "ghost" }),
+        "py-4 px-1 flex flex-col items-center rounded-lg gap-1"
+      )}
+    >
+      <Icon className="w-6 h-6" />
+      <div className="text-sm">{title}</div>
+    </a>
+  );
+}
